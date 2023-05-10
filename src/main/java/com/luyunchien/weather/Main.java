97a8889ca2f77182273d813c69bc4b9b18476a72
Yun-Chien Lu <luyunchien@gmail.com>
2023-05-10 22:38:16 +0100

    update printed results

--- src/main/java/com/luyunchien/weather/Main.java
+++ src/main/java/com/luyunchien/weather/Main.java
@@ -1,8 +1,8 @@
 package com.luyunchien.weather;
 
 import com.google.gson.Gson;
-import lombok.ToString;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
@@ -15,24 +15,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) throws Exception {
         if (args.length != 1) {
             throw new RuntimeException("Usage: weather.jar SEARCH_QUERY");
         }
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
             List<WeatherInfo> data = getAllWeather(args[0], httpClient);
-            System.out.println(data.size());
+            System.out.println(String.format("Found %d results", data.size()));
             for(WeatherInfo info : data){
-                System.out.println(info.name);
+                System.out.println(info.toString());
             }
         }
-
     }
 
     public static List<WeatherInfo> getAllWeather(String search, CloseableHttpClient httpClient) throws Exception{
         int page = 1;
         List<WeatherInfo> data = new ArrayList<>();
-        while (true){
+        while (true) {
             WeatherResponse response = getWeather(search, page, httpClient);
             if(response.data.isEmpty()) break;
             data.addAll(response.data);
@@ -46,23 +46,23 @@ public class Main {
         builder.addParameter("name", search);
         builder.addParameter("page", String.valueOf(page));
         URI uri = builder.build();
-        System.out.println(uri);
 
         HttpGet request = new HttpGet(uri);
         try (CloseableHttpResponse httpResponse = httpClient.execute(request)) {
-            System.out.println(httpResponse.getStatusLine().getStatusCode());
+            int statusCode = httpResponse.getStatusLine().getStatusCode();
+            if (statusCode != HttpStatus.SC_OK) {
+                throw new RuntimeException(String.format("Unexpected status code: %d", statusCode));
+            }
             HttpEntity entity = httpResponse.getEntity();
             String result = EntityUtils.toString(entity);
-            System.out.println(result);
 
             Gson gson = new Gson();
             WeatherResponse weatherResponse = gson.fromJson(result, WeatherResponse.class);
-            System.out.println(weatherResponse);
             return weatherResponse;
         }
     }
 
-    @ToString
+    @SuppressWarnings("unused")
     static class WeatherResponse{
         int page;
         int per_page;
@@ -71,11 +71,15 @@ public class Main {
         List<WeatherInfo> data;
     }
 
-    @ToString
     static class WeatherInfo{
         String name;
         String weather;
         List<String> status;
+
+        @Override
+        public String toString() {
+            return String.format("City = %s; Weather = %s; Status = %s", name, weather, status);
+        }
     }
 }
 
